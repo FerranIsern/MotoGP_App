a8b046591125aa11ebf04ba454de0c63af11d1ef
FerranIsern <ferran@isern.blasco.cat>
2017-12-15 16:40:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ferra/motogp_test/playerselect.java
@@ -0,0 +1,13 @@
+package com.example.ferra.motogp_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class playerselect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playerselect);
+    }
+}
